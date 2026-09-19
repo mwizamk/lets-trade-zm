a5bd469f045262faa6c6a3789dbf4aeff33f3c84
mwizamk <plugmk6@gmail.com>
2026-09-19 03:47:54 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword, 
-  onAuthStateChanged, 
-  signOut 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { 
-  getFirestore, 
-  doc, 
-  setDoc, 
-  getDoc 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfTSZI9HNxiveExkl8y5ZujU3RhoGupds",
@@ -22,20 +11,7 @@ const firebaseConfig = {
   appId: "1:1052285066127:web:6884f38b2642613959c9fa"
 };
 
-// Initialize Firebase App
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Export Auth & Firestore functions
-export { 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword, 
-  onAuthStateChanged, 
-  signOut,
-  doc,
-  setDoc,
-  getDoc
-};
+export { doc, setDoc, getDoc, updateDoc };
