fa5587ee0c65cb700a8ee2d761d55dbe2b1e20d9
mwizamk <plugmk6@gmail.com>
2026-09-19 03:52:52 +0200

    Add Firestore utilities and export app instance

--- firebase.js
+++ firebase.js
@@ -1,6 +1,18 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  updateDoc,
+  addDoc,
+  collection,
+  query,
+  where,
+  onSnapshot 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfTSZI9HNxiveExkl8y5ZujU3RhoGupds",
@@ -11,7 +23,20 @@ const firebaseConfig = {
   appId: "1:1052285066127:web:6884f38b2642613959c9fa"
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export { doc, setDoc, getDoc, updateDoc };
+
+// Export Common Firestore Utilities
+export { 
+  doc, 
+  setDoc, 
+  getDoc, 
+  updateDoc, 
+  addDoc, 
+  collection, 
+  query, 
+  where, 
+  onSnapshot 
+};
