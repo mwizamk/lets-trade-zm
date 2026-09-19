7d43f625b3b4b6c02641cea2168779de50de8869
mwizamk <plugmk6@gmail.com>
2026-09-19 04:07:02 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,6 +8,7 @@ import {
   getDoc, 
   updateDoc,
   addDoc,
+  deleteDoc,
   collection,
   query,
   where,
@@ -23,18 +24,17 @@ const firebaseConfig = {
   appId: "1:1052285066127:web:6884f38b2642613959c9fa"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Export Common Firestore Utilities
 export { 
   doc, 
   setDoc, 
   getDoc, 
   updateDoc, 
   addDoc, 
+  deleteDoc,
   collection, 
   query, 
   where, 
