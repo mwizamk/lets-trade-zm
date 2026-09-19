dd6f5896d844afa0b9d4448cc148456aa1ceed89
mwizamk <plugmk6@gmail.com>
2026-09-19 02:57:09 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { 
   getAuth, 
@@ -14,26 +13,14 @@ import {
   getDoc 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Replace with your actual Firebase project credentials from the Firebase Console
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCfTSZI9HNxiveExkl8y5ZujU3RhoGupds",
-    authDomain: "lets-trade-zm-6849d.firebaseapp.com",
-    projectId: "lets-trade-zm-6849d",
-    storageBucket: "lets-trade-zm-6849d.firebasestorage.app",
-    messagingSenderId: "1052285066127",
-    appId: "1:1052285066127:web:6884f38b2642613959c9fa"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const firebaseConfig = {
+  apiKey: "AIzaSyCfTSZI9HNxiveExkl8y5ZujU3RhoGupds",
+  authDomain: "lets-trade-zm-6849d.firebaseapp.com",
+  projectId: "lets-trade-zm-6849d",
+  storageBucket: "lets-trade-zm-6849d.firebasestorage.app",
+  messagingSenderId: "1052285066127",
+  appId: "1:1052285066127:web:6884f38b2642613959c9fa"
+};
 
 // Initialize Firebase App
 const app = initializeApp(firebaseConfig);
@@ -42,7 +29,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Export Auth functions for app.js to use
+// Export Auth & Firestore functions
 export { 
   createUserWithEmailAndPassword, 
   signInWithEmailAndPassword, 
@@ -52,4 +39,3 @@ export {
   setDoc,
   getDoc
 };
-
